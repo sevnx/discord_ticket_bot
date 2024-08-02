013a391f2da1f799b2fe44755821f3d49d508064
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2024-08-02 12:31:33 +0200

    [database] Add DB script

---
+++ database/script.sql
@@ -0,0 +1,200 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.3
+-- Dumped by pg_dump version 16.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE IF EXISTS dc_ticket;
+--
+-- Name: dc_ticket; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE dc_ticket WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_GB.UTF-8';
+
+
+ALTER DATABASE dc_ticket OWNER TO postgres;
+
+\connect dc_ticket
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: servers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.servers (
+    id bigint NOT NULL,
+    setup_complete boolean DEFAULT false NOT NULL,
+    ticket_channel_id bigint,
+    unclaimed_category_id bigint,
+    claimed_category_id bigint,
+    ticket_message_id bigint
+);
+
+
+ALTER TABLE public.servers OWNER TO postgres;
+
+--
+-- Name: subjects; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.subjects (
+    id bigint NOT NULL,
+    server_id bigint NOT NULL,
+    name character varying(100) NOT NULL,
+    channel_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.subjects OWNER TO postgres;
+
+--
+-- Name: subjects_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.subjects ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.subjects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: tickets; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.tickets (
+    ticket_id integer NOT NULL,
+    channel_id bigint NOT NULL,
+    author_id bigint NOT NULL,
+    subject_id bigint,
+    is_open boolean DEFAULT true,
+    server_id bigint NOT NULL
+);
+
+
+ALTER TABLE public.tickets OWNER TO postgres;
+
+--
+-- Name: tickets_ticket_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.tickets_ticket_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.tickets_ticket_id_seq OWNER TO postgres;
+
+--
+-- Name: tickets_ticket_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.tickets_ticket_id_seq OWNED BY public.tickets.ticket_id;
+
+
+--
+-- Name: tickets ticket_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tickets ALTER COLUMN ticket_id SET DEFAULT nextval('public.tickets_ticket_id_seq'::regclass);
+
+
+--
+-- Name: subjects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.subjects_id_seq', 8, true);
+
+
+--
+-- Name: tickets_ticket_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.tickets_ticket_id_seq', 2, true);
+
+
+--
+-- Name: servers servers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.servers
+    ADD CONSTRAINT servers_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: subjects subjects_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.subjects
+    ADD CONSTRAINT subjects_pkey PRIMARY KEY (id, server_id);
+
+
+--
+-- Name: subjects subjects_server_id_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.subjects
+    ADD CONSTRAINT subjects_server_id_name_key UNIQUE (server_id, name);
+
+
+--
+-- Name: tickets tickets_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tickets
+    ADD CONSTRAINT tickets_pkey PRIMARY KEY (ticket_id);
+
+
+--
+-- Name: tickets tickets_servers_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tickets
+    ADD CONSTRAINT tickets_servers_id_fk FOREIGN KEY (server_id) REFERENCES public.servers(id);
+
+
+--
+-- Name: tickets tickets_subjects_id_server_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tickets
+    ADD CONSTRAINT tickets_subjects_id_server_id_fk FOREIGN KEY (subject_id, server_id) REFERENCES public.subjects(id, server_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
